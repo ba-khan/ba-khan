4f00c85a9e504dfc17693b8d04fad5ba6a688db0
javierperezferrada <javierperezferrada@gmail.com>
2016-01-05 11:19:20 -0300

    Login con Khan Academy

--- bakhan/urls.py
+++ bakhan/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'bakhanapp.views.login', name='login'),
+    url(r'^$', 'bakhanapp.views.log', name='log'),
     url(r'^inicio/', 'bakhanapp.views.inicio', name='inicio'),
+    url(r'^run/', 'bakhanapp.views.run_tests', name='run_tests'),
+    url(r'^acceso/denegado/', 'bakhanapp.views.denegado', name='denegado'),
 ]
 #hola
--- bakhanapp/views.py
+++ bakhanapp/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render,HttpResponseRedirect,render_to_response
 from .forms import loginForm
 #from django.shortcuts import render_to_response
 
+import cgi
+import rauth
+import SimpleHTTPServer
+import SocketServer
+import time
+import webbrowser
+
+def log(request):
+    return render(request, 'log.html')
+
+def denegado(request):
+    return render(request, 'denegado.html')
+
 def login(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
@@ -23,3 +36,113 @@ def login(request):
 
 def inicio(request):
     return render_to_response('inicio.html',)
+
+
+# You can get a CONSUMER_KEY and CONSUMER_SECRET for your app here:
+# http://www.khanacademy.org/api-apps/register
+CONSUMER_KEY = 'uhPpmjAMXqKwVYyJ' #clave generada para Alonsoccer
+CONSUMER_SECRET = 'cY8yWWjaKBVPUQfd' #clave generada para Alonsoccer
+    
+CALLBACK_BASE = '127.0.0.1'
+SERVER_URL = 'http://www.khanacademy.org'
+    
+DEFAULT_API_RESOURCE = '/api/v1/playlists'
+VERIFIER = None
+    
+
+# Create the callback server that's used to set the oauth verifier after the
+# request token is authorized.
+def create_callback_server():
+    class CallbackHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+        def do_GET(self):
+            global VERIFIER
+
+            params = cgi.parse_qs(self.path.split('?', 1)[1],
+                keep_blank_values=False)
+            VERIFIER = params['oauth_verifier'][0]
+
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('OAuth request token fetched and authorized;' +
+                ' you can close this window. B!tch.')
+            #webbrowser.open('http://www.google.cl')
+            
+            
+
+        def log_request(self, code='-', size='-'):
+            pass
+
+    server = SocketServer.TCPServer((CALLBACK_BASE, 0), CallbackHandler)
+    return server
+
+
+# Make an authenticated API call using the given rauth session.
+#/api/v1/user?userId=&username=javierperezferrada&email=
+def get_api_resource(session):
+    resource_url = '/api/v1/user?userId=&username=&email='
+
+    url = SERVER_URL + resource_url
+    split_url = url.split('?', 1)
+    params = {}
+
+    # Separate out the URL's parameters, if applicable.
+    if len(split_url) == 2:
+        url = split_url[0]
+        params = cgi.parse_qs(split_url[1], keep_blank_values=False)
+
+    start = time.time()
+    response = session.get(url, params=params)
+    end = time.time()
+    json_response = response.json()
+    email = json_response['email']
+    if email == 'javierperezferrada@gmail.com':
+         return True
+    else:
+        return False
+    
+
+def run_tests(request):
+    global CONSUMER_KEY, CONSUMER_SECRET, SERVER_URL
+    
+    # Set consumer key, consumer secret, and server base URL from user input or
+    # use default values.
+    CONSUMER_KEY = CONSUMER_KEY
+    CONSUMER_SECRET = CONSUMER_SECRET
+    SERVER_URL = SERVER_URL
+
+    # Create an OAuth1Service using rauth.
+    service = rauth.OAuth1Service(
+           name='test',
+           consumer_key=CONSUMER_KEY,
+           consumer_secret=CONSUMER_SECRET,
+           request_token_url=SERVER_URL + '/api/auth2/request_token',
+           access_token_url=SERVER_URL + '/api/auth2/access_token',
+           authorize_url=SERVER_URL + '/api/auth2/authorize',
+           base_url=SERVER_URL + '/api/auth2')
+
+    callback_server = create_callback_server()
+
+    # 1. Get a request token.
+    request_token, secret_request_token = service.get_request_token(
+        params={'oauth_callback': 'http://%s:%d/' %
+            (CALLBACK_BASE, callback_server.server_address[1])})
+    
+    # 2. Authorize your request token.
+    authorize_url = service.get_authorize_url(request_token)
+    webbrowser.open(authorize_url)
+
+    callback_server.handle_request()
+    callback_server.server_close()
+
+    # 3. Get an access token.
+    session = service.get_auth_session(request_token, secret_request_token,
+        params={'oauth_verifier': VERIFIER})
+
+    # Repeatedly prompt user for a resource and make authenticated API calls.
+    if get_api_resource(session):
+        return HttpResponseRedirect('/inicio')
+    else:
+        return HttpResponseRedirect('/acceso/denegado')
+
+
