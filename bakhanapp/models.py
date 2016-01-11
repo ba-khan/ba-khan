e03fc7dfbd7977adba53e9e6c981eed8b690c3f6
tvargasvicencio <tvargas.vicencio@gmail.com>
2016-01-11 12:31:04 -0300

    Actualizacion models.py

--- bakhanapp/models.py
+++ bakhanapp/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Institution(models.Model):
-    id_institution = models.IntegerField(primary_key=True)
+    id_institution = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     city = models.CharField(max_length=30)
-    address = models.CharField(max_length=100)
-    phone = models.CharField(max_length=30)
+    address = models.CharField(max_length=100,null=True)
+    phone = models.CharField(max_length=15,null=True)
     
     def __unicode__(self): # __unicode__ on Python 2
         return self.name
     
 class Administrator(models.Model):
-    kaid_administrator = models.CharField(max_length=30,primary_key=True)
+    kaid_administrator = models.CharField(max_length=40,primary_key=True)
     name = models.CharField(max_length=50)
-    mail = models.EmailField()
+    email = models.EmailField()
     id_institution = models.ForeignKey(Institution)
     
     def __unicode__(self): # __unicode__ on Python 2
@@ -24,11 +24,11 @@ class Administrator(models.Model):
         pass
 
 class Student(models.Model):
-    kaid_student = models.CharField(max_length=30,primary_key=True)
+    kaid_student = models.CharField(max_length=40,primary_key=True)
     name = models.CharField(max_length=50)
-    mail = models.EmailField(max_length=50)
-    tutor_name = models.CharField(max_length=50)
-    tutor_mail = models.EmailField()
+    email = models.EmailField(max_length=50)
+    tutor_name = models.CharField(max_length=50,null=True)
+    tutor_email = models.EmailField(max_length=50,null=True)
     points = models.IntegerField()
     
     def __unicode__(self): # __unicode__ on Python 2
@@ -39,6 +39,7 @@ class Student(models.Model):
         pass
     
 class Group(models.Model):
+    id_group = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
     kaid_student_tutor = models.ForeignKey(Student,null=True)
@@ -53,132 +54,177 @@ class Group_Student(models.Model):
     kaid_student = models.ForeignKey(Student)
     
 class Teacher(models.Model):
-    kaid_teacher = models.CharField(max_length=30,primary_key=True)
+    kaid_teacher = models.CharField(max_length=40,primary_key=True)
     name = models.CharField(max_length=50)
     id_institution = models.ForeignKey(Institution)
+    email = models.EmailField(max_length=50)
     
     def __unicode__(self): # __unicode__ on Python 2
         return self.name
 
 class Class(models.Model):
-    id_institution = models.ForeignKey(Institution)
-    level = models.CharField(max_length=50)
+    id_class = models.AutoField(primary_key=True)
+    id_institution = models.ForeignKey(Institution, related_name='id_institution_class')
+    level = models.IntegerField()
     letter = models.CharField(max_length=2)
-    kaid_teacher = models.ForeignKey(Teacher)
+    year = models.IntegerField()
+
+    class Meta:
+        unique_together = ('id_institution', 'level', 'letter', 'year')
     
 class Student_Class(models.Model):
+    id_student_class = models.AutoField(primary_key=True)
     id_class = models.ForeignKey(Class)
     kaid_student = models.ForeignKey(Student)
+    
+    class Meta:
+        unique_together = ('id_class', 'kaid_student')
 
 class Subject(models.Model):
-    id_subject = models.CharField(max_length=50,primary_key=True)
-    name = models.CharField(max_length=50)
+    id_subject_name = models.CharField(max_length=50,primary_key=True)
+    name_spanish = models.CharField(max_length=50)
     
     def __unicode__(self): # __unicode__ on Python 2
-        return self.name_esp
+        return self.name_spanish
 
 class Class_Subject(models.Model):
+    id_class_subject = models.AutoField(primary_key=True)
     id_class = models.ForeignKey(Class)
-    id_subject = models.ForeignKey(Subject)
+    id_subject_name = models.ForeignKey(Subject)
     kaid_teacher = models.ForeignKey(Teacher)
+    
+    class Meta:
+        unique_together = ('id_class', 'id_subject_name')
 
 class Chapter(models.Model):
-    id_chapter_name = models.CharField(max_length=50,primary_key=True)
-    id_subject = models.ForeignKey(Subject)
-    name_esp = models.CharField(max_length=50)
+    id_chapter_name = models.CharField(max_length=150,primary_key=True)
+    id_subject_name = models.ForeignKey(Subject)
+    name_spanish = models.CharField(max_length=150)
     
     def __unicode__(self): # __unicode__ on Python 2
-        return self.name_esp
+        return self.name_spanish
 
 class Topic(models.Model):
-    id_topic_name = models.CharField(max_length=50,primary_key=True)
+    id_topic_name = models.CharField(max_length=150,primary_key=True)
     id_chapter_name = models.ForeignKey(Chapter)
-    name_esp = models.CharField(max_length=50)
+    name_spanish = models.CharField(max_length=150)
     
     def __unicode__(self): # __unicode__ on Python 2
-        return self.name_esp
+        return self.name_spanish
 
     
 class Subtopic(models.Model):
-    id_subtopic_name = models.CharField(max_length=50,primary_key=True)
+    id_subtopic_name = models.CharField(max_length=150,primary_key=True)
     id_topic_name = models.ForeignKey(Topic)
-    name_esp = models.CharField(max_length=50)
+    name_spanish = models.CharField(max_length=150)
     
     def __unicode__(self): # __unicode__ on Python 2
-        return self.name_esp
+        return self.name_spanish
 
 class Skill(models.Model):
-    id_skill_name = models.CharField(max_length=100,primary_key=True)
-    name_esp = models.CharField(max_length=100)
+    id_skill_name = models.CharField(max_length=150,primary_key=True)
+    name_spanish = models.CharField(max_length=150)
     id_subtopic_name = models.ForeignKey(Subtopic)
     
     def __unicode__(self): # __unicode__ on Python 2
-        return self.name_esp
+        return self.name_spanish
 
 class Assesment_Config(models.Model):
+    id_assesment_config = models.AutoField(primary_key=True)
     kaid_teacher = models.ForeignKey(Teacher)
-    id_subject = models.ForeignKey(Subject)
+    id_subject_name = models.ForeignKey(Subject)
     approval_percentage = models.IntegerField()
     top_score = models.IntegerField()
-    assesment_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     
-class Assesment_skill(models.Model):
-    id_assesment_conf = models.ForeignKey(Assesment_Config)
+class Assesment_Skill(models.Model):
+    id_assesment_skill = models.AutoField(primary_key=True)
+    id_assesment_config = models.ForeignKey(Assesment_Config)
     id_skill_name = models.ForeignKey(Skill)
     
+    class Meta:
+        unique_together = ('id_assesment_config', 'id_skill_name')
+    
 class Assesment(models.Model):
+    id_assesment = models.AutoField(primary_key=True)
     id_assesment_conf = models.ForeignKey(Assesment_Config)
     start_date = models.DateField()
     end_date = models.DateField()
+    id_group = models.ForeignKey(Group)
 
 class Grade(models.Model):
+    id_grade = models.AutoField(primary_key=True)
     kaid_student = models.ForeignKey(Student)
     id_assesment = models.ForeignKey(Assesment)
     grade = models.IntegerField()
-    performance_point = models.IntegerField()
+    teacher_grade = models.IntegerField(null=True)
+    performance_points = models.IntegerField()
     effort_points = models.IntegerField()
+    
+    class Meta:
+        unique_together = ('kaid_student', 'id_assesment')
 
 class Video(models.Model):
-    id_video_name = models.CharField(max_length=100,primary_key=True)
-    name_esp = models.CharField(max_length=100)
+    id_video_name = models.CharField(max_length=150,primary_key=True)
+    name_spanish = models.CharField(max_length=150)
     id_subtopic_name = models.ForeignKey(Subtopic)
     
     def __unicode__(self): # __unicode__ on Python 2
         return self.name_esp
 
 class Student_Skill(models.Model):
+    id_student_skill = models.AutoField(primary_key=True)
     id_skill_name = models.ForeignKey(Skill)
     kaid_student = models.ForeignKey(Student)
     total_done = models.IntegerField()
     total_correct = models.IntegerField()
     streak = models.IntegerField()
     longest_streak = models.IntegerField()
-    exercises_states = models.CharField(max_length=30)
-    exercise_progress = models.CharField(max_length=30)
-
+    skill_progress = models.CharField(max_length=50)
+    total_hints = models.IntegerField()
+    struggling = models.BooleanField()
+    
+    class Meta:
+        unique_together = ('id_skill_name', 'kaid_student')
+        
 class Student_Video(models.Model):
+    id_student_video = models.AutoField(primary_key=True)
     id_video_name = models.ForeignKey(Video)
     kaid_student = models.ForeignKey(Student)
     total_seconds_watched = models.IntegerField()
     total_points_earned = models.IntegerField()
     last_second_watched = models.IntegerField()
     is_video_complete = models.BooleanField()
+    
+    class Meta:
+        unique_together = ('id_video_name', 'kaid_student')
 
 class Skill_Attempt(models.Model):
+    id_skill_attempt = models.AutoField(primary_key=True)
     id_skill_name = models.ForeignKey(Skill)
     kaid_student = models.ForeignKey(Student)
     errors = models.IntegerField()
-    mission = models.CharField(max_length=100)
+    mission = models.CharField(max_length=150,null=True)
     time_taken = models.IntegerField()
     count_hints = models.IntegerField()
     skipped = models.BooleanField()
+    points_earned = models.IntegerField()
     date = models.DateField()
+    correct = models.BooleanField()
     
 class Video_Playing(models.Model):
-    id_skill_name = models.ForeignKey(Skill)
+    id_video_playing = models.AutoField(primary_key=True)
+    id_video_name = models.ForeignKey(Video)
     kaid_student = models.ForeignKey(Student)
     seconds_watched = models.IntegerField()
     points_earned = models.IntegerField()
     last_second_watched = models.IntegerField()
     is_video_complete = models.BooleanField()
+    date = models.DateField()
+
+class Skill_Progress(models.Model):
+    id_skill_progress = models.AutoField(primary_key=True)
+    id_skill_name = models.ForeignKey(Skill)
+    to_level = models.CharField(max_length=50)
+    from_level = models.CharField(max_length=50)
     date = models.DateField()
