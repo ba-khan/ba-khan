ede24fbed58fb53e48767e17445259d341950c72
javierperezferrada <javierperezferrada@gmail.com>
2016-01-12 09:52:46 -0300

    Update models.py

--- bakhanapp/models.py
+++ bakhanapp/models.py
@@ -227,7 +227,7 @@ class Video_Playing(models.Model):
 
 class Skill_Progress(models.Model):
     id_skill_progress = models.AutoField(primary_key=True)
-    id_skill_name = models.ForeignKey(Skill)
+    id_student_skill = models.ForeignKey(Student_Skill)
     to_level = models.CharField(max_length=50)
     from_level = models.CharField(max_length=50)
     date = models.DateField()
