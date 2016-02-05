85d9357cfa08cf762ff7134e5fdc0131605d4881
tvargasvicencio <tvargas.vicencio@gmail.com>
2016-02-05 18:02:44 -0300

    App lista
    Con detalles al editar nombres (se crea otro administrador al editar un
    nombre), ya que estos se están ocupando como pk porque no hay definida
    una pk realmente.

--- ManagementTeam/urls.py
+++ ManagementTeam/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.getAdministrators, name='getAdministrators'),
-    url(r'^nueva$', views.newAdministrator, name='nuevo_administrador'),
-    url(r'^eliminar/$', views.deleteAdministrator, name='eliminar_administrador'),
-    url(r'^editar/$', views.editAdministrator, name='editar_administrador'),
+    url(r'^guardar$', views.saveAdministrator, name='guardar_administrador'),
+    url(r'^eliminar$', views.deleteAdministrator, name='eliminar_administrador'),
 
 ]
