9cf7832607f964ae07dfc896428465da933c3e7e
tvargasvicencio <tvargas.vicencio@gmail.com>
2016-02-09 14:44:20 -0300

    Actualizacion login

--- login/views.py
+++ login/views.py
@@ -225,10 +225,10 @@ def authenticate(request):
     
     # Repeatedly prompt user for a resource and make authenticated API calls.
     if get_api_resource(session,request):
-        server.server_close()
+        #server.server_close()
         return HttpResponseRedirect("/inicio")
     else:
-        server.server_close()
+        #server.server_close()
         return HttpResponseRedirect("/access/rejected/ %}")
     
 
@@ -274,3 +274,4 @@ def run_tests():
     #while(True):
     #    get_api_resource(session)
     return session
+
