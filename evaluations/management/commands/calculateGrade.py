b2456873c48b662e3651fc6ee4a0615aacf62411
javierperezferrada <javierperezferrada@gmail.com>
2016-02-10 16:49:38 -0300

    calcula y guarda el puntaje obtenido por el alumno segun su nivel de dominio con la orm de django

--- evaluations/management/commands/calculateGrade.py
+++ evaluations/management/commands/calculateGrade.py
@@ -1,4 +1,5 @@
 import time
+import json
 from django.core.management.base import BaseCommand, CommandError
 from bakhanapp.models import Grade,Assesment,Assesment_Config,Assesment_Skill,Student_Skill,Skill_Progress
 class Command(BaseCommand):
@@ -14,6 +15,7 @@ class Command(BaseCommand):
             approval_percentage = Assesment_Config.objects.get(pk=assesment.id_assesment_conf_id).approval_percentage
             skills = Assesment_Skill.objects.filter(id_assesment_config_id=assesment.id_assesment_conf_id).values('id_skill_name_id')
             grades_involved = Grade.objects.filter(id_assesment_id=assesment.pk)
+            #print grades_involved
             for grade in grades_involved:
                 grade.performance_points = getSkillPoints(grade.kaid_student_id,skills,assesment.start_date,assesment.end_date)
                 grade.save()
@@ -24,17 +26,18 @@ def getSkillPoints(kaid_student,configured_skills,t_begin,t_end):
     scores={'unstarted':0,'struggling':20,'practiced':40,'mastery1':60,'mastery2':80,'mastery3':100}
     points = 0
     for skill in configured_skills:
+        #print 'id_skill_name_id %s'%(skill['id_skill_name_id'])
         try: 
-            id_student_skills = Student_Skill.objects.filter(id_skill_name_id=skill,kaid_student_id=kaid_student).values('id_student_skill')
+            #print skill['id_skill_name_id']
+            id_student_skills = Student_Skill.objects.filter(id_skill_name_id=skill['id_skill_name_id'],kaid_student_id=kaid_student)#.values('id_student_skill')
+            for id_student_skill in id_student_skills:
+                id_student_skill = id_student_skill.id_student_skill
         except: 
-            print "no data"
-
+            print "no data id_student_skills"
         try: 
-            last_level = Skill_Progress.objects.filter(id_student_skill_id=id_student_skills,date__gte = '2015-01-01',date__lte = '2016-02-09').latest('date').values('to_level')
+            last_level = Skill_Progress.objects.filter(id_student_skill_id=id_student_skill,date__gte = t_begin,date__lte = t_end).values('to_level').latest('date')
+            points = points + scores[last_level['to_level']]
         except: 
             print 'no hay registros'
-            last_level = 'unstarted'
-        points = points + scores[last_level]
     points = points / len(configured_skills)
-    print points
     return points
