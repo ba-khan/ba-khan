a6964cf9864b18a772ee0035cd24934210e7fa5e
javierperezferrada <javierperezferrada@gmail.com>
2016-02-10 12:37:43 -0300

    grade_involved

--- evaluations/management/commands/calculateGrade.py
+++ evaluations/management/commands/calculateGrade.py
@@ -1,6 +1,6 @@
 import time
 from django.core.management.base import BaseCommand, CommandError
-from bakhanapp.models import Grade,Assesment,Assesment_Config
+from bakhanapp.models import Grade,Assesment,Assesment_Config,Assesment_Skill
 class Command(BaseCommand):
     help = 'Calcula notas al pasar la fecha de la evaluacion'
 
@@ -9,6 +9,9 @@ class Command(BaseCommand):
         currentDate = time.strftime("%Y-%m-%d") #fecha actual.
         assesments = Assesment.objects.filter(end_date__lte=currentDate)
         for assesment in assesments:
-            print assesment
             approval_percentage = Assesment_Config.objects.get(pk=assesment.id_assesment_conf_id).approval_percentage
-            print approval_percentage
+            skills = Assesment_Skill.objects.filter(id_assesment_config_id=assesment.id_assesment_conf_id).values('id_skill_name_id')
+            for s in skills:
+                print s
+            grades_involved = Grade.objects.filter(id_assesment_id=assesment.pk)
+            print grades_involved[0].grade
