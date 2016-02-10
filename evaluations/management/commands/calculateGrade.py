f9d90e9f83f4260b464da6b70d5328b921f88a78
javierperezferrada <javierperezferrada@gmail.com>
2016-02-10 12:25:34 -0300

    assesment vencidas y porcentaje de aprovacion

--- evaluations/management/commands/calculateGrade.py
+++ evaluations/management/commands/calculateGrade.py
@@ -1,6 +1,6 @@
 import time
 from django.core.management.base import BaseCommand, CommandError
-from bakhanapp.models import Grade,Assesment
+from bakhanapp.models import Grade,Assesment,Assesment_Config
 class Command(BaseCommand):
     help = 'Calcula notas al pasar la fecha de la evaluacion'
 
@@ -8,4 +8,7 @@ class Command(BaseCommand):
         #funcion que se ejecutara al hacer python manage.py calculateGrade
         currentDate = time.strftime("%Y-%m-%d") #fecha actual.
         assesments = Assesment.objects.filter(end_date__lte=currentDate)
-        print assesments
+        for assesment in assesments:
+            print assesment
+            approval_percentage = Assesment_Config.objects.get(pk=assesment.id_assesment_conf_id).approval_percentage
+            print approval_percentage
