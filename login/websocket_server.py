abf9e9066b0a99ea14e2e8102dcd1007ac41cd8d
jordanbarriap <jordanbarriap@gmail.com>
2016-02-11 14:58:20 -0400

    Revert "Comentarios sobre import de django"
    This reverts commit a50d63587100c5dd2a201f7ffadbfbeedabd0307.

--- login/websocket_server.py
+++ login/websocket_server.py
@@ -1,44 +1,44 @@
 # -*- encoding: utf-8 -*-
 # -*- coding: utf-8 -*-
-#from django.shortcuts import render,HttpResponseRedirect,render_to_response, redirect,HttpResponse
-#from django.template.context import RequestContext
-#from .forms import loginForm
-#from django.contrib.auth import  login,authenticate,logout
-#from django.contrib.auth.decorators import login_required,permission_required
-#from django.contrib.auth.forms import AuthenticationForm
-#from django.contrib.auth.models import User
-#from django.contrib import auth
-#from django.db.models import Count
+from django.shortcuts import render,HttpResponseRedirect,render_to_response, redirect,HttpResponse
+from django.template.context import RequestContext
+from .forms import loginForm
+from django.contrib.auth import  login,authenticate,logout
+from django.contrib.auth.decorators import login_required,permission_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.db.models import Count
 
-#from django import template
-#from bakhanapp.models import Assesment_Skill
-#register = template.Library()
+from django import template
+from bakhanapp.models import Assesment_Skill
+register = template.Library()
 
-#from bakhanapp.models import Class
-#from bakhanapp.models import Teacher
-#from bakhanapp.models import Skill
-#from bakhanapp.models import Skill_Progress
-#from bakhanapp.models import Student
-#from bakhanapp.models import Student_Class
-#from bakhanapp.models import Student_Video
-#from bakhanapp.models import Student_Skill
-#from bakhanapp.models import Video
-#from bakhanapp.models import Video_Playing
-#from bakhanapp.models import Skill_Attempt
-#from bakhanapp.models import Assesment_Skill
-#from bakhanapp.models import Class_Subject
-#from bakhanapp.models import Assesment
-#from bakhanapp.models import Assesment_Config
-#from bakhanapp.models import Subtopic_Skill
-#from bakhanapp.models import Subtopic_Video
-#from bakhanapp.models import Grade,Skill
-#from bakhanapp.models import Student_Skill
-#from bakhanapp.models import Skill_Progress
-#from bakhanapp.models import Subject
-#from bakhanapp.models import Chapter
-#from bakhanapp.models import Topic
-#from bakhanapp.models import Subtopic
-#from bakhanapp.models import Subtopic_Skill
+from bakhanapp.models import Class
+from bakhanapp.models import Teacher
+from bakhanapp.models import Skill
+from bakhanapp.models import Skill_Progress
+from bakhanapp.models import Student
+from bakhanapp.models import Student_Class
+from bakhanapp.models import Student_Video
+from bakhanapp.models import Student_Skill
+from bakhanapp.models import Video
+from bakhanapp.models import Video_Playing
+from bakhanapp.models import Skill_Attempt
+from bakhanapp.models import Assesment_Skill
+from bakhanapp.models import Class_Subject
+from bakhanapp.models import Assesment
+from bakhanapp.models import Assesment_Config
+from bakhanapp.models import Subtopic_Skill
+from bakhanapp.models import Subtopic_Video
+from bakhanapp.models import Grade,Skill
+from bakhanapp.models import Student_Skill
+from bakhanapp.models import Skill_Progress
+from bakhanapp.models import Subject
+from bakhanapp.models import Chapter
+from bakhanapp.models import Topic
+from bakhanapp.models import Subtopic
+from bakhanapp.models import Subtopic_Skill
 
 import datetime
 
@@ -56,8 +56,8 @@ import sys
 from pprint import pprint
 import codecs
 from lib2to3.fixer_util import String
-#from django.core import serializers
-#from django.db import connection
+from django.core import serializers
+from django.db import connection
 
 import random
 from random import randint
