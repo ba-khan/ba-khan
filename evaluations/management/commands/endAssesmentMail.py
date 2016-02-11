69aa598a21cedd46c61717fd69193a976d993d97
javierperezferrada <javierperezferrada@gmail.com>
2016-02-11 10:11:11 -0300

    habilidades por cada evaluacion vencida

--- evaluations/management/commands/endAssesmentMail.py
+++ evaluations/management/commands/endAssesmentMail.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 import json
 from django.core.management.base import BaseCommand, CommandError
-from bakhanapp.models import Grade,Assesment,Assesment_Config,Assesment_Skill,Student_Skill,Skill_Progress
+from bakhanapp.models import Grade,Assesment,Assesment_Config,Assesment_Skill,Student_Skill,Skill_Progress,Skill
 
 class Command(BaseCommand):
     help = 'Envia un mail con la nota y las variables de empegno y desempegno'
@@ -9,9 +9,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         #funcion que se ejecutara al hacer python manage.py calculateGrade
         lastDate = date.today() - timedelta(days=1)
-        assesments = Assesment.objects.filter(end_date=lastDate).values('name','grade__grade',
-            'grade__kaid_student_id','grade__performance_points')#inner join django 1-N desde 1 hacia N
+        #assesments = Assesment.objects.filter(end_date=lastDate).values('id_assesment','name','grade__grade',
+        #    'grade__kaid_student_id','grade__performance_points')#inner join django 1-N desde 1 hacia N
+        assesments = Assesment.objects.filter(end_date=lastDate)
         for assesment in assesments: 
-            print assesment['grade__grade']
-            #grades = Grade.objects.filter()
+            #print assesment.id_assesment_conf_id
+            skills = getSelectedSkills(assesment.id_assesment_conf_id) 
+            print skills
+
+def getSelectedSkills(id_assesment_config):
+    skills = Skill.objects.filter(assesment_skill__id_assesment_config_id=id_assesment_config).values('name_spanish','assesment_skill__id_assesment_config_id')
+    return skills
         
