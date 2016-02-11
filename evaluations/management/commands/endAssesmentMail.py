d4c84c727a962c4a9e37bed5fecb2d0b2bb9e703
javierperezferrada <javierperezferrada@gmail.com>
2016-02-11 10:20:26 -0300

    skills,kaid_student,perf_point y grades en consulta con orm

--- evaluations/management/commands/endAssesmentMail.py
+++ evaluations/management/commands/endAssesmentMail.py
@@ -9,13 +9,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         #funcion que se ejecutara al hacer python manage.py calculateGrade
         lastDate = date.today() - timedelta(days=1)
-        #assesments = Assesment.objects.filter(end_date=lastDate).values('id_assesment','name','grade__grade',
-        #    'grade__kaid_student_id','grade__performance_points')#inner join django 1-N desde 1 hacia N
-        assesments = Assesment.objects.filter(end_date=lastDate)
+        assesments = Assesment.objects.filter(end_date=lastDate).values('id_assesment_conf_id','id_assesment','name','grade__grade',
+            'grade__kaid_student_id','grade__performance_points')#inner join django 1-N desde 1 hacia N
+        #assesments = Assesment.objects.filter(end_date=lastDate)
+        conf = 0 
         for assesment in assesments: 
-            #print assesment.id_assesment_conf_id
-            skills = getSelectedSkills(assesment.id_assesment_conf_id) 
-            print skills
+            if conf != assesment['id_assesment_conf_id']:
+                skills = getSelectedSkills(assesment['id_assesment_conf_id']) 
+                conf = assesment['id_assesment_conf_id']
+                print skills
 
 def getSelectedSkills(id_assesment_config):
     skills = Skill.objects.filter(assesment_skill__id_assesment_config_id=id_assesment_config).values('name_spanish','assesment_skill__id_assesment_config_id')
