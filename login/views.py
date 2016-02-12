c854f55c2a3d9f03523ece546a3e0e337c2a7c53
jordanbarriap <jordanbarriap@gmail.com>
2016-02-12 11:13:52 -0400

    Servidor Websocket habilitado
    Servidor Websocket habilitado en puerto 9001, vía la ejecución en python
    del script websocket_server_python.py. Este archivo debe ser ejecutado
    en el servidor en el cual los clientes soliciten conexión, y asegurarse
    que se ejecute de forma infinita.

--- login/views.py
+++ login/views.py
@@ -66,216 +66,26 @@ import time
 
 from websocket_server import WebsocketServer
 
-#FUNCIONA POR FAVOOOOOOOOOOOOOOOOOOR
-
 
 def login(request):
-    wsPort = (randint(52000,55000))
-    return render_to_response('login.html', {'wsPort': wsPort}, context_instance=RequestContext(request))
+    return render_to_response('login.html', context_instance=RequestContext(request))
 
 def rejected(request):
     return render(request, 'rejected.html')
 
 
-
-
-CONSUMER_KEY = 'uMCFkRw7QSJ3WkLs' #clave generada para don UTPs
-CONSUMER_SECRET = 'tH8vhEBstXe6jFyG' #clave generada para don UTPs
-    
-CALLBACK_BASE = '146.83.216.177' #IP Servidor
-SERVER_URL = 'http://www.khanacademy.org'
-SERVER_URL2 = 'http://es.khanacademy.org'
-    
-DEFAULT_API_RESOURCE = '/api/v1/playlists'
-VERIFIER = None
-    
-
-# Create the callback server that's used to set the oauth verifier after the
-# request token is authorized.
-def create_callback_server():
-    class CallbackHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-        def do_GET(self):
-            global VERIFIER
-
-            params = cgi.parse_qs(self.path.split('?', 1)[1],
-                keep_blank_values=False)
-            VERIFIER = params['oauth_verifier'][0]
-
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.end_headers()
-            self.wfile.write("""<html lang="es">
-                            <head>
-                                <title>BA-Khan</title>                                                               
-                                <script>
-                                    myWindow = window.open("", "_self", "myWindow", "width=200, height=100")
-                                    function closeWin() {
-                                        myWindow.close();
-                                    }
-                                </script>                          
-                            </head>
-                              <body>                             
-                                <script>closeWin()</script>                       
-                              </body>                           
-                            </html>""")
-            #webbrowser.open('http://www.google.cl')
-
-        def log_request(self, code='-', size='-'):
-            pass
-
-    server = SocketServer.TCPServer((CALLBACK_BASE, 0), CallbackHandler) #Ocupar puerto 0 (en vez de 53707) para puerto random
-    return server
-
-
-# Make an authenticated API call using the given rauth session.
-#/api/v1/user?userId=&username=javierperezferrada&email=
-
-def get_api_resource(session,request):
-    resource_url = '/api/v1/user?userId=&username=&email='
-
-    url = SERVER_URL + resource_url
-    split_url = url.split('?', 1)
-    params = {}
-
-    # Separate out the URL's parameters, if applicable.
-    if len(split_url) == 2:
-        url = split_url[0]
-        params = cgi.parse_qs(split_url[1], keep_blank_values=False)
-
-    #start = time.time()
-    response = session.get(url, params=params)
-    #end = time.time()
-    json_response = response.json()
-    email = json_response['email']
-    #username = json_response['username']
-    user = auth.authenticate(username=email, password=email)
-    if user:
-        auth.login(request, user)
-        return True
-    else:
-        user = User.objects.create_user(username=email,email=email,password=email)
-        user.save()
-        return False
-
-def authenticate(request,wsPort):
-    global CONSUMER_KEY, CONSUMER_SECRET, SERVER_URL
-    
-    # Set consumer key, consumer secret, and server base URL from user input or
-    # use default values.
-    CONSUMER_KEY = CONSUMER_KEY
-    CONSUMER_SECRET = CONSUMER_SECRET
-    SERVER_URL = SERVER_URL
-
-    # Create an OAuth1Service using rauth.
-    service = rauth.OAuth1Service(
-           name='test',
-           consumer_key=CONSUMER_KEY,
-           consumer_secret=CONSUMER_SECRET,
-           request_token_url=SERVER_URL + '/api/auth2/request_token',
-           access_token_url=SERVER_URL + '/api/auth2/access_token',
-           authorize_url=SERVER_URL + '/api/auth2/authorize',
-           base_url=SERVER_URL + '/api/auth2')
-
-    callback_server = create_callback_server()
-
-    # 1. Get a request token.
-    request_token, secret_request_token = service.get_request_token(
-        params={'oauth_callback': 'http://%s:%d/' %
-            (CALLBACK_BASE, callback_server.server_address[1])})
-    
-    # 2. Authorize your request token.
-    authorize_url = service.get_authorize_url(request_token)
-    #return HttpResponseRedirect(authorize_url)
-    print authorize_url
-    #webbrowser.open(authorize_url, new=1)
-    
-    #server.send_message_to_all("WATZAAAAAAAAAA")
-    # Called for every client connecting (after handshake)
-    def new_client(client, server):
-        print("New client connected and was given id %d" % client['id'])
-        server.send_message_to_all("Hey all, a new client has joined us")
-    
-    
-    # Called for every client disconnecting
-    def client_left(client, server):
-        print("Client(%d) disconnected" % client['id'])
-    
-    
-    # Called when a client sends a message
-    def message_received(client, server, message):
-        if len(message) > 200:
-            message = message[:200]+'..'
-        print("Client(%d) said: %s" % (client['id'], message))
-        if (message=="listoco"):
-            server.send_message_to_all("url:"+authorize_url)
-            server.shutdown()
-            server=""
-    
-    PORT=int(wsPort)
-    SERVERHOST="146.83.216.177"
-    server = WebsocketServer(PORT, SERVERHOST)
-    #server.shutdown()
-    server.set_fn_new_client(new_client)
-    server.set_fn_client_left(client_left)
-    server.set_fn_message_received(message_received)
-    server.run_forever()
-    
-    callback_server.handle_request()
-    callback_server.server_close()
-    callback_server=""
-
-    # 3. Get an access token.
-    session = service.get_auth_session(request_token, secret_request_token,
-        params={'oauth_verifier': VERIFIER})
-    
-    server.shutdown()
-    
-    # Repeatedly prompt user for a resource and make authenticated API calls.
-    if get_api_resource(session,request):
-        return HttpResponseRedirect("/inicio")
-    else:
-        return HttpResponseRedirect("/access/rejected/ %}")
-    
-
-def run_tests():
-    global CONSUMER_KEY, CONSUMER_SECRET, SERVER_URL
-    
-    # Set consumer key, consumer secret, and server base URL from user input or
-    # use default values.
-    CONSUMER_KEY = CONSUMER_KEY
-    CONSUMER_SECRET = CONSUMER_SECRET
-    SERVER_URL = SERVER_URL
-
-    # Create an OAuth1Service using rauth.
-    service = rauth.OAuth1Service(
-           name='test',
-           consumer_key=CONSUMER_KEY,
-           consumer_secret=CONSUMER_SECRET,
-           request_token_url=SERVER_URL + '/api/auth2/request_token',
-           access_token_url=SERVER_URL + '/api/auth2/access_token',
-           authorize_url=SERVER_URL + '/api/auth2/authorize',
-           base_url=SERVER_URL + '/api/auth2')
-
-    callback_server = create_callback_server()
-
-    # 1. Get a request token.
-    request_token, secret_request_token = service.get_request_token(
-        params={'oauth_callback': 'http://%s:%d/' %
-            (CALLBACK_BASE, callback_server.server_address[1])})
-    
-    # 2. Authorize your request token.
-    authorize_url = service.get_authorize_url(request_token)
-    #urlib2.urlopen(authorize_url)
-
-    callback_server.handle_request()
-    callback_server.server_close()
-
-    # 3. Get an access token.
-    session = service.get_auth_session(request_token, secret_request_token,
-        params={'oauth_verifier': VERIFIER})
-
-    # Repeatedly prompt user for a resource and make authenticated API calls.
-    #print
-    #while(True):
-    #    get_api_resource(session)
-    return session
+def authenticateUser(request):
+    authorized = False
+    if request.method == 'GET':
+        args = request.GET
+        username = args["username"]
+        email = args["email"]
+        print username+" "+email
+        user = auth.authenticate(username=email, password=email)
+        if user:
+            auth.login(request, user)
+            authorized = True
+        else:
+            user = User.objects.create_user(username=email,email=email,password=email)
+            user.save()
+    return HttpResponse(authorized)
