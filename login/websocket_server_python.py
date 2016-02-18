8b208f6b6bcaf0d825d7e9eb1ceff8efcb4c6746
jordanbarriap <jordanbarriap@gmail.com>
2016-02-18 00:37:14 -0400

    Server no Run Forever

--- login/websocket_server_python.py
+++ login/websocket_server_python.py
@@ -241,7 +241,7 @@ def message_received(client, server, message):
         json_data = json.dumps(url_data)
         server.send_message(client,json_data)
         
-        #callback_server.handle_request()
+        callback_server.handle_request()
         #callback_server.server_close()
         #callback_server=""
         #callback_server.serve_forever()
@@ -269,7 +269,7 @@ server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
 server.run_forever()
-callback_server.serve_forever()
+#callback_server.serve_forever()
 #callback_server.serve_forever()
 
 
