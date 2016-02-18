76ffbd692668db79bcff1d8911d6622c51eb7e9f
jordanbarriap <jordanbarriap@gmail.com>
2016-02-18 00:27:26 -0400

    Authentification Server Run Forever

--- login/websocket_server_python.py
+++ login/websocket_server_python.py
@@ -269,7 +269,7 @@ server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
 server.run_forever()
-#callback_server.serve_forever()
+callback_server.serve_forever()
 #callback_server.serve_forever()
 
 
