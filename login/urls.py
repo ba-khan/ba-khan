7014eb0ab8170f8b795debbf10d66267a63f4c09
javierperezferrada <javierperezferrada@gmail.com>
2016-02-26 15:13:06 -0300

    rejected1

--- login/urls.py
+++ login/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.login, name='login'),
     #url(r'^authenticate/(?P<wsPort>[0-9]+)', views.authenticate, name='authenticate'),
-    #url(r'^access/rejected/', views.rejected, name='rejected'),
+    url(r'^access/rejected/', views.rejected, name='rejected'),
     url(r'^salir$', logout, {'next_page': '/'}),
     url(r'^authenticate/$', views.authenticateUser, name='authenticateUser'), 
     #url(r'^historial/$', views.getMakedGroup, name='getMakedGroup'),
