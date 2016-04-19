0dcb1a0dc19e042cd026d2c2ca56932e9f826477
javierperezferrada <javierperezferrada@gmail.com>
2016-04-19 08:58:27 -0300

    muestra todos los cursos, agrupaciones, y contactos del establecimiento a los administradores

--- bakhanapp/views.py
+++ bakhanapp/views.py
@@ -92,10 +92,15 @@ def teacher(request):
 
 @login_required()
 def getTeacherClasses(request):
+    #funcion que es llamada con la url:/inicio
     request.session.set_expiry(timeSleep)
     #print request.user.user_profile.kaid
     #Esta funcion entrega todos los cursos que tiene a cargo el profesor que se encuentra logueado en el sistema
-    classes = Class.objects.filter(id_class__in=Class_Subject.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_class'))
+    #Si la persona logeada es un administrador, muestra todos los cursos de su establecimiento.
+    if(request.user.has_perm('bakhanapp.isAdmin')):
+        classes = Class.objects.filter(id_institution_id=Teacher.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_institution_id'))
+    else:
+        classes = Class.objects.filter(id_class__in=Class_Subject.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_class'))
     N = ['kinder','1ro basico','2do basico','3ro basico','4to basico','5to basico','6to basico','7mo basico','8vo basico','1ro medio','2do medio','3ro medio','4to medio']
     for i in range(len(classes)):
         classes[i].level = N[int(classes[i].level)] 
@@ -213,7 +218,10 @@ def getSkillsCorrect(grade_id):
 def getClassStudents(request, id_class):
     #Esta funcion entrega todos los estudiantes que pertenecen a un curso determinado y carga el dashboard
     request.session.set_expiry(timeSleep)#10 minutos
-    classes = Class.objects.filter(id_class__in=Class_Subject.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_class'))
+    if(request.user.has_perm('bakhanapp.isAdmin')):
+        classes = Class.objects.filter(id_institution_id=Teacher.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_institution_id'))
+    else:
+        classes = Class.objects.filter(id_class__in=Class_Subject.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_class'))
     N = ['kinder','1ro basico','2do basico','3ro basico','4to basico','5to basico','6to basico','7mo basico','8vo basico','1ro medio','2do medio','3ro medio','4to medio']
     try:
         clas = Class.objects.get(id_class=id_class)
