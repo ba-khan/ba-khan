c49f356ccd97d1fbafd99b10b18e1ccbaa6e427e
tvargasvicencio <tvargas.vicencio@gmail.com>
2016-05-10 12:55:55 -0300

    Avance backend Nomina

--- classRoster/views.py
+++ classRoster/views.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render,HttpResponseRedirect,render_to_response, redirect, HttpResponse
 from django.template.context import RequestContext
+from django.core.management.base import BaseCommand, CommandError
 from bakhanapp.forms import AssesmentConfigForm,AssesmentForm
 from django.contrib.auth import  login,authenticate,logout
 from django.contrib.auth.decorators import login_required,permission_required
@@ -21,11 +22,122 @@ from bakhanapp.models import Class
 from bakhanapp.models import Student
 from bakhanapp.models import Student_Class
 from bakhanapp.models import Class_Subject
+from bakhanapp.models import Institution
 
 register = template.Library()
 from configs import timeSleep
+import datetime
+import cgi
+import rauth
+import SimpleHTTPServer
+import SocketServer
+import time
+import webbrowser
+import psycopg2
+import requests
+from collections import OrderedDict
+import json
+import simplejson
+import sys
+from pprint import pprint
+import codecs
+from lib2to3.fixer_util import String
+from django.core import serializers
+from django.db import connection
 
-import simplejson as json
+import random
+
+import urlparse
+
+CALLBACK_BASE = '127.0.0.1'
+SERVER_URL = 'https://www.khanacademy.org'
+SERVER_URL2 = 'https://es.khanacademy.org'
+    
+DEFAULT_API_RESOURCE = '/api/v1/playlists'
+VERIFIER = None
+
+def run_tests(identifier,passw, CONSUMER_KEY, CONSUMER_SECRET):
+    global SERVER_URL
+    SERVER_URL = SERVER_URL
+
+    # Create an OAuth1Service using rauth.
+    service = rauth.OAuth1Service(
+           name='test',
+           consumer_key=CONSUMER_KEY,
+           consumer_secret=CONSUMER_SECRET,
+           request_token_url=SERVER_URL + '/api/auth2/request_token',
+           access_token_url=SERVER_URL + '/api/auth2/access_token',
+           authorize_url=SERVER_URL + '/api/auth2/authorize',
+           base_url=SERVER_URL + '/api/auth2')
+
+    callback_server = create_callback_server()
+
+    # 1. Get a request token.
+    request_token, secret_request_token = service.get_request_token(
+        params={'oauth_callback': SERVER_URL +'/api/auth/default_callback'}) 
+    
+    noClickParams = {'oauth_token':request_token, 'identifier':identifier, 'password':passw}
+    
+    # 2. Authorize your request token.
+    authorize_url = service.get_authorize_url(request_token)
+
+    post_url=SERVER_URL +'/api/auth2/authorize'
+
+    r = requests.post(post_url, data=noClickParams)
+
+    access_url = urlparse.parse_qs(urlparse.urlparse(r.url).query)
+    oauth_verifier_raw = access_url["oauth_verifier"][0]
+    oauth_verifier = oauth_verifier_raw.encode('ascii','ignore')
+    callback_server.server_close()
+
+    # 3. Get an access token.
+    session = service.get_auth_session(request_token, secret_request_token,
+        params={'oauth_verifier': oauth_verifier})
+
+    # Repeatedly prompt user for a resource and make authenticated API calls.
+    return session
+
+def get_api_resource2(sessions,llamada,server):
+    url = server + llamada
+    split_url = url.split('?', 1)
+    params = {}
+    if len(split_url) == 2:
+        url = split_url[0]
+        params = cgi.parse_qs(split_url[1], keep_blank_values=False)
+    start = time.time()
+    responses = sessions.get(url, params=params)
+    encoded_response=responses.text.encode(sys.stdout.encoding,errors='replace')
+    end = time.time()
+    return encoded_response
+
+def create_callback_server():
+    class CallbackHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+        def do_GET(self):
+            global VERIFIER
+            logging.debug("kk")
+            params = cgi.parse_qs(self.path.split('?', 1)[1],
+                keep_blank_values=False)
+            VERIFIER = params['oauth_verifier'][0]
+
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('OAuth request token fetched and authorized;' +
+                ' you can close this window. B!tch.')
+            #webbrowser.open('http://www.google.cl')
+
+        def log_request(self, code='-', size='-'):
+            pass
+
+    server = SocketServer.TCPServer((CALLBACK_BASE, 0), CallbackHandler)
+    return server
+
+def searchTeacher(session,newTeacher):
+    llamada = "/api/v1/user?username="+newTeacher
+    jason = get_api_resource2(session,llamada,SERVER_URL)
+    source = unicode(jason, 'ISO-8859-1')
+    data = simplejson.loads(source)
+    return data
 
 
 @permission_required('bakhanapp.isAdmin', login_url="/")
@@ -54,6 +166,19 @@ def newTeacherClass(request):
     if request.method == 'POST':
         newTeacher = request.POST["username"]
         print newTeacher
+
+        inst = Institution.objects.get(id_institution=Teacher.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_institution_id'))
+
+        keys = inst.key
+        secrets = inst.secret
+        identifiers = inst.identifier
+        passes = inst.password
+    
+        sessions = run_tests(identifiers,passes,keys,secrets)
+        data = searchTeacher(sessions,newTeacher)
+        print data["username"]
+       
+
         return HttpResponse("Nuevo profesor "+newTeacher)
 
 @permission_required('bakhanapp.isAdmin', login_url="/")
