4f7bb49ab4a912b6ba2c4649b6990576f77ab680
iuliusfdp <iuliusfdp@hotmail.com>
2016-05-19 10:24:30 -0300

    bakhanDB

--- bakhan/settings.py
+++ bakhan/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'bakhan.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test2',
+        'NAME': 'bakhanDB',
         'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': '146.83.216.177',
