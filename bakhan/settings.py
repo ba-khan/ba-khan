a294fc5b5a77041c31f4eed6835135341df54d88
tvargasvicencio <tvargas.vicencio@gmail.com>
2016-06-12 16:39:19 -0400

    Incidencias 13, 16 y 18

--- bakhan/settings.py
+++ bakhan/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uy=!=x&yfcv)a_bbk19xx_4c1w@x$j)g5=i(u99)1qo$+p4)2e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TEMPLATE_DEBUG = False
-#DEBUG = True
+#DEBUG = TEMPLATE_DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-#ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 TEMPLATE_DIRS = (
     join(BASE_DIR,  'templates'),
