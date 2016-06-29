fcf1deef31799be86256a5e8d86e0235134c989b
iuliusfdp <iuliusfdp@hotmail.com>
2016-06-29 10:43:22 -0300

    id_institution=3

--- populate/management/commands/populateStudentSkill.py
+++ populate/management/commands/populateStudentSkill.py
@@ -409,7 +409,7 @@ class Command(BaseCommand):
         #meter los parametros anteriores en alguna parte de la base de datos
 
         #institution = Institution.objects.all()
-        institution = Institution.objects.filter(id_institution=7)
+        institution = Institution.objects.filter(id_institution=3)
 
 
         for inst in institution:
