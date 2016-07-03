53e97002101b4b48bc42af1a551c8980060a1aa7
iuliusfdp <iuliusfdp@hotmail.com>
2016-07-03 14:48:47 -0400

    encoding al top

--- populate/management/commands/populateData.py
+++ populate/management/commands/populateData.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 # -*- encoding: utf-8 -*-
 # -*- coding: utf-8 -*-
 from django.shortcuts import render,HttpResponseRedirect,render_to_response, redirect,HttpResponse
