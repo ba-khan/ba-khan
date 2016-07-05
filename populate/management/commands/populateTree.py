7ff8570ea4beeeb2333376fdb27db8320851fc27
iuliusfdp <iuliusfdp@hotmail.com>
2016-07-05 16:36:50 -0400

    update tree

--- populate/management/commands/populateTree.py
+++ populate/management/commands/populateTree.py
@@ -261,32 +261,32 @@ def poblar_topictree(session,buscar, reemplazar):
     id_subtopic_videos = 1
     cant_chapters = len(data["children"][1]["children"])
     for i in range(0,cant_chapters):
-        '''
+
         aux1 = (data["children"][1]["children"][i]["slug"])
         aux2 = (data["children"][1]["children"][i]["translated_title"])
         aux3 = (data["children"][1]["slug"])
         new_chapter = Chapter(id_chapter_name=aux1,name_spanish=aux2,id_subject_name_id=aux3, index=i)
         new_chapter.save()
-       	'''
+
         cant_topic = len(data["children"][1]["children"][i]["children"])
         
         for j in range(0,cant_topic):
         	
-            #aux1 = data["children"][1]["children"][i]["children"][j]["slug"]
-            #aux2 = data["children"][1]["children"][i]["children"][j]["translated_title"]
-            #aux3 = data["children"][1]["children"][i]["slug"]
-            #new_topic = Topic(id_topic_name=aux1,name_spanish=aux2,id_chapter_name_id=aux3, index=j)
-            #new_topic.save()
+            aux1 = data["children"][1]["children"][i]["children"][j]["slug"]
+            aux2 = data["children"][1]["children"][i]["children"][j]["translated_title"]
+            aux3 = data["children"][1]["children"][i]["slug"]
+            new_topic = Topic(id_topic_name=aux1,name_spanish=aux2,id_chapter_name_id=aux3, index=j)
+            new_topic.save()
         	
             cant_subtopic = len(data["children"][1]["children"][i]["children"][j]["children"])
             for k in range(0,cant_subtopic):
-            	'''
+
                 aux1 = data["children"][1]["children"][i]["children"][j]["children"][k]["slug"]
                 aux2 = data["children"][1]["children"][i]["children"][j]["children"][k]["translated_title"]
                 aux3 = data["children"][1]["children"][i]["children"][j]["slug"]
                 new_subtopic = Subtopic(id_subtopic_name=aux1,name_spanish=aux2,id_topic_name_id=aux3, index=k)
                 new_subtopic.save()
-            	'''                
+               
                 cant_videos = len(data["children"][1]["children"][i]["children"][j]["children"][k]["child_data"])
                 #print cant_videos
 
@@ -294,7 +294,7 @@ def poblar_topictree(session,buscar, reemplazar):
                     #consulta = """UPDATE bakhanapp_video SET name_spanish = '"""+data_topictree["children"][1]["children"][i]["children"][j]["children"][k]["children"][l]["translated_title"].replace(buscar,reemplazar)+"""' WHERE id_video_name = '"""+data_topictree["children"][1]["children"][i]["children"][j]["children"][k]["children"][l]["id"]+"""';"""
                     #cur.execute(consulta)
                     #conn.commit()
-                	'''
+         
                     if (data["children"][1]["children"][i]["children"][j]["children"][k]["child_data"][l]["kind"]=="Exercise"):
                         aux1 = (data["children"][1]["children"][i]["children"][j]["children"][k]["child_data"][l]["id"])
                         aux2 = (data["children"][1]["children"][i]["children"][j]["children"][k]["translated_title"])
@@ -327,7 +327,7 @@ def poblar_topictree(session,buscar, reemplazar):
                         except:
                             pass
                         #id_subtopic_videos+=1
-        			'''            
+        	          
                 skills = get_api_resource2(session,"/api/v1/topic/"+data["children"][1]["children"][i]["children"][j]["children"][k]["slug"]+"/exercises",SERVER_URL2)
                 source = unicode(skills, 'ISO-8859-1')
                 data_skills = simplejson.loads(source)
