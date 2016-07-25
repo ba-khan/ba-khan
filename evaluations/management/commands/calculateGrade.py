7dabcb0eb8bba851d769ff133d5b7bedbebe01c5
julian <iuliusfdp@hotmail.com>
2016-07-25 11:30:03 -0400

    update skill log calculategrade

--- evaluations/management/commands/calculateGrade.py
+++ evaluations/management/commands/calculateGrade.py
@@ -230,8 +230,8 @@ class Command(BaseCommand):
                         inc_aux = 0
                         skill_progress_aux = 'unstarted'
                         try:#id04
-                            incorrect = Skill_Attempt.objects.filter(Q(kaid_student=grade.kaid_student)&Q(id_skill_name_id=sk_lg.id_skill_name)&Q(correct=False)&Q(task_type__startswith="mastery")&(Q(skipped=False)|Q(skipped=True))&Q(date__range=(assesment.start_date,assesment.end_date))).values('kaid_student_id').annotate(incorrect=Count('kaid_student_id'))
-                            correct = Skill_Attempt.objects.filter(kaid_student=grade.kaid_student,id_skill_name_id=sk_lg.id_skill_name,correct=True,date__range=(assesment.start_date,assesment.end_date)).values('kaid_student_id').annotate(correct=Count('kaid_student_id'))
+                            incorrect = Skill_Attempt.objects.filter(Q(kaid_student=grade.kaid_student)&Q(id_skill_name_id=sk_lg.id_skill_name_id)&Q(correct=False)&Q(task_type__startswith="mastery")&(Q(skipped=False)|Q(skipped=True))&Q(date__range=(assesment.start_date,assesment.end_date))).values('kaid_student_id').annotate(incorrect=Count('kaid_student_id'))
+                            correct = Skill_Attempt.objects.filter(kaid_student=grade.kaid_student,id_skill_name_id=sk_lg.id_skill_name_id,correct=True,date__range=(assesment.start_date,assesment.end_date)).values('kaid_student_id').annotate(correct=Count('kaid_student_id'))
                             sklprgrs = Student_Skill.objects.filter(kaid_student_id=grade.kaid_student,id_skill_name_id=sk_lg.id_skill_name_id,skill_progress__date__range=(assesment.start_date,assesment.end_date)).values('kaid_student','id_student_skill','skill_progress__to_level','skill_progress__date').order_by('kaid_student','id_student_skill','-skill_progress__date').distinct('kaid_student','id_student_skill')
                             is_struggling = Student_Skill.objects.filter(kaid_student_id=grade.kaid_student,id_skill_name_id=sk_lg.id_skill_name_id).values('struggling')
                         except Exception as e:
@@ -261,7 +261,7 @@ class Command(BaseCommand):
                             cor_aux = 0
                         try:#id05
                             update_skill_log = Skill_Log(id_skill_log = sk_lg.id_skill_log,
-                                    id_skill_name_id = sk_lg.id_skill_name,
+                                    id_skill_name_id = sk_lg.id_skill_name_id,
                                     correct = cor_aux,
                                     incorrect =  inc_aux,
                                     skill_progress = skill_progress_aux,
