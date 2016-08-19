5433dc7b6fc98dd3c77d70b99f64ad811809d1ee
iuliusfdp <iuliusfdp@hotmail.com>
2016-08-19 19:56:35 -0300

    addTests

--- Test/suite.py
+++ Test/suite.py
@@ -18,7 +18,7 @@ class Test_Suite(unittest.TestCase):
          
         # suite of TestCases
         self.suite = unittest.TestSuite()
-        self.suite.addTest([            
+        self.suite.addTests([            
             unittest.defaultTestLoader.loadTestsFromTestCase(TestLoginUtp.TestLoginUtp),
             unittest.defaultTestLoader.loadTestsFromTestCase(TestPautasTestUtp.TestPautasTestUtp),
             unittest.defaultTestLoader.loadTestsFromTestCase(TestVercursoVergruposContactos1erMedioA.TestVercursoVergruposContactos1erMedioA)
