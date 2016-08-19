3bcd3a09bd39d8052d992eb6e8a5d5920ed968e8
tvargasvicencio <tvargas.vicencio@gmail.com>
2016-08-19 10:25:25 -0400

    Fix Test VerCursoBonusMenor

--- Test/TestVerCursoBonusMenor.py
+++ Test/TestVerCursoBonusMenor.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from pyvirtualdisplay import Display
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
