160ecc4280a5aca1cb1d4d665024d69809925ad5
iuliusfdp <iuliusfdp@hotmail.com>
2016-08-19 22:13:26 -0300

    display stop

--- Test/TestPautasTestUtp.py
+++ Test/TestPautasTestUtp.py
@@ -49,7 +49,8 @@ class TestPautasTestUtp(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
-        display.popen.kill()
+        #display.popen.kill()
+        display.stop()
 
 if __name__ == "__main__":
     unittest.main()
