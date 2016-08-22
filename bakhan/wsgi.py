5199050d9c1a7fd45c29262b589defb3abeb710a
iuliusfdp <iuliusfdp@hotmail.com>
2016-08-22 16:11:17 -0300

    wsgi

--- bakhan/wsgi.py
+++ bakhan/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+path = '/var/www/html/bakhanprueba'
+if path not in sys.path:
+	sys.path.append(path)
+	
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bakhan.settings")
 
 application = get_wsgi_application()
