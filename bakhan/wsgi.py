027e97fe7f3f75d0dbd112966db5b7f5ed6b7070
Melombus <tvargas.vicencio@gmail.com>
2016-08-22 17:15:32 -0300

    Cambio solicitado en wsgi.py

--- bakhan/wsgi.py
+++ bakhan/wsgi.py
@@ -1,21 +1,14 @@
 """
 WSGI config for bakhan project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-path = os.path.abspath(os.path.dirname(__name__))
-if path not in sys.path:
-	sys.path.append(path)
-	
-os.environ["DJANGO_SETTINGS_MODULE"]= "bakhan.settings"
+os.environ["DJANGO_SETTINGS_MODULE"]="bakhan.settings"
 
 application = get_wsgi_application()
