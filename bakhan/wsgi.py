d3c03a848e6c3cab94bf82cec950e04061c339e0
iuliusfdp <iuliusfdp@hotmail.com>
2016-08-22 16:43:01 -0300

    wsgi fix

--- bakhan/wsgi.py
+++ bakhan/wsgi.py
@@ -16,6 +16,6 @@ path = os.path.abspath(os.path.dirname(__name__))
 if path not in sys.path:
 	sys.path.append(path)
 	
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bakhan.settings")
+os.environ["DJANGO_SETTINGS_MODULE"]= "bakhan.settings"
 
 application = get_wsgi_application()
