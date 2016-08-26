695931f0368a35d75f45e284717d944ffa79ae87
julian <iuliusfdp@hotmail.com>
2016-08-26 12:02:15 -0300

    update urls

--- configuracion/urls.py
+++ configuracion/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.getAdministrators, name='getSettings'),
-    url(r'^guardar$', views.saveAdministrator, name='guardar_horario'),
-    url(r'^nuevo$', views.newAdministrator, name='nuevo_horario'),
-    url(r'^eliminar$', views.deleteAdministrator, name='eliminar_horario'),
+    #url(r'^$', views.getAdministrators, name='getSettings'),
+    #url(r'^guardar$', views.saveAdministrator, name='guardar_horario'),
+    #url(r'^nuevo$', views.newAdministrator, name='nuevo_horario'),
+    #url(r'^eliminar$', views.deleteAdministrator, name='eliminar_horario'),
 
 ]
