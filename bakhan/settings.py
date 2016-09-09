a4a6a09327526025fd91d5ff9f0729895d8c5c48
harlleyaugusto <harlleyaugusto@gmail.com>
2016-09-09 10:19:18 -0300

    teste 2

--- bakhan/settings.py
+++ bakhan/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.8/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
-#teste
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from os.path import join
