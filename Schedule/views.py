a87cf3267c9b9aef697360819233992788b632e6
julian <iuliusfdp@hotmail.com>
2016-09-12 12:00:18 -0300

    label con el curso en definir horario

--- Schedule/views.py
+++ Schedule/views.py
@@ -46,7 +46,14 @@ def getSchedules(request, id_class):
 	schedules = Schedule.objects.filter(id_institution_id=teacher.id_institution_id).order_by('start_time')
 	teacher = Class_Subject.objects.filter(id_class_id=id_class).values('kaid_teacher_id')
 	class_schedule = Class_Schedule.objects.filter(kaid_teacher_id=teacher[0]['kaid_teacher_id'])
-	return render_to_response('schedules.html', {'spanish_classroom':spanish_classroom, 'schedules': schedules, 'class_schedule': class_schedule, 'id_class':id_class, 'teacher':teacher, 'isTeacher':isTeacher}, context_instance=RequestContext(request))
+	if(request.user.has_perm('bakhanapp.isAdmin')):
+		classes = Class.objects.filter(id_institution_id=Teacher.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_institution_id')).order_by('level','letter')
+	else:
+		classes = Class.objects.filter(id_class__in=Class_Subject.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_class')).order_by('level','letter')
+	N = ['kinder','1ro basico','2do basico','3ro basico','4to basico','5to basico','6to basico','7mo basico','8vo basico','1ro medio','2do medio','3ro medio','4to medio']
+	for i in range(len(classes)):
+		classes[i].nivel = N[int(classes[i].level)] 
+	return render_to_response('schedules.html', {'spanish_classroom':spanish_classroom, 'schedules': schedules, 'class_schedule': class_schedule, 'id_class':id_class, 'teacher':teacher, 'isTeacher':isTeacher, 'classes':classes}, context_instance=RequestContext(request))
 
 @login_required()
 def saveScheduleClass(request, id_class):
