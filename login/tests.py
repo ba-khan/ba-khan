b30b9e5c2f1694084d6d56854df6e2b22fcf1e6d
harlleyaugusto <harlleyaugusto@gmail.com>
2016-09-21 16:08:37 -0300

    Apply a test in views of login

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,15 @@
+
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import Client
+
+class LoginTest(TestCase):
+
+    def test_authenticateUser(self):
+
+        client = Client()
+        response = self.client.post('/authenticate/', {'username': 'fred', 'email': 'secret', 'kaid': '12121', 'avatar_url':'asdafs'})
+        self.assertEqual(response.content, 'True')
+
