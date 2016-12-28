1c06a6997123dbe2c6bce5e480ae18ce31075305
julian <iuliusfdp@hotmail.com>
2016-12-28 12:38:42 -0300

    curriculum

--- curriculum/views.py
+++ curriculum/views.py
@@ -216,7 +216,66 @@ def getCurriculumNivel(request, id_chapter_mineduc):
 		json_dict={"capitulos":mision}
 		json_data = json.dumps(json_dict)
 
-		return render_to_response('curriculumnivel.html', {'topic_mineduc':tpmin, 'nombrecapitulo':nombrechapter[0]['name'], 'json_data':json_data}, context_instance=RequestContext(request))
+		topictree_json={}
+		topictree_json['checkbox']={'keep_selected_style':False}
+		topictree_json['plugins']=['checkbox','search']
+		topictree=[]
+
+		topictree_json_vid={}
+		topictree_json_vid['checkbox']={'keep_selected_style':False}
+		topictree_json_vid['plugins']=['checkbox','search']
+		topictree_vid=[]
+
+		subjects=Subject.objects.all()
+		for subject in subjects:
+		    subject_obj={"id": subject.id_subject_name, "parent":"#", "text": subject.name_spanish, "state": {"opened":"true"}, "icon":"false"}
+		    topictree.append(subject_obj)
+		    topictree_vid.append(subject_obj)
+		subject_chapter=Chapter.objects.exclude(index=None).order_by('index')
+		for chapter in subject_chapter:
+		    chapter_obj={"id":chapter.id_chapter_name, "parent": chapter.id_subject_name_id, "text":chapter.name_spanish, "icon":"false"}
+		    topictree.append(chapter_obj)
+		    topictree_vid.append(chapter_obj)
+		chapter_topic=Topic.objects.exclude(index=None).order_by('index')
+		for topic in chapter_topic:
+		    topic_obj={"id":topic.id_topic_name, "parent": topic.id_chapter_name_id, "text":topic.name_spanish, "icon":"false"}
+		    topictree.append(topic_obj)
+		    topictree_vid.append(topic_obj)
+		topic_subtopic=Subtopic.objects.exclude(index=None).order_by('index')
+		for subtopic in topic_subtopic:
+		    subtopic_obj={"id":subtopic.id_subtopic_name, "parent": subtopic.id_topic_name_id, "text":subtopic.name_spanish, "icon":"false"}
+		    topictree.append(subtopic_obj)
+		    topictree_vid.append(subtopic_obj)
+		subtopic_skill=Subtopic_Skill.objects.filter(id_subtopic_name_id__in=topic_subtopic).select_related('id_skill_name')
+		#id=0
+		subtopic_video=Subtopic_Video.objects.filter(id_subtopic_name_id__in=topic_subtopic).select_related('id_video_name')
+		for video in subtopic_video:
+			video_id=video.id_subtopic_video
+			video_obj={"id":video_id, "parent":video.id_subtopic_name_id, "text": video.id_video_name.name_spanish, "data":{"video_id":video.id_video_name.id_video_name}, "index":video.id_video_name.index}
+			sorted(video_obj, key=video_obj.get)
+			topictree_vid.append(video_obj)
+
+		for skill in subtopic_skill:
+		    skill_id=skill.id_subtopic_skill
+		    skill_obj={"id":skill_id, "parent":skill.id_subtopic_name_id, "text": skill.id_skill_name.name_spanish, "data":{"skill_id":skill.id_skill_name.id_skill_name}, "icon":"false", "index":skill.id_skill_name.index}
+		    sorted(skill_obj, key=skill_obj.get)
+		    topictree.append(skill_obj)
+		    topictree_vid.append(skill_obj)
+		    #id=id+1
+		subtopic_video=Subtopic_Video.objects.filter(id_subtopic_name_id__in=topic_subtopic).select_related('id_video_name')
+		for video in subtopic_video:
+			video_id=video.id_subtopic_video
+			video_obj={"id":video_id, "parent":video.id_subtopic_name_id, "text": video.id_video_name.name_spanish, "data":{"video_id":video.id_video_name.id_video_name}, "index":video.id_video_name.index}
+			sorted(video_obj, key=video_obj.get)
+			topictree.append(video_obj)
+
+		topictree_json_vid['core']={'data':topictree_vid}
+		topictree_json_string_video=json.dumps(topictree_json_vid)
+
+		topictree_json['core']={'data':topictree}
+		topictree_json_string=json.dumps(topictree_json)
+
+		return render_to_response('curriculumnivel.html', {'topic_mineduc':tpmin, 'nombrecapitulo':nombrechapter[0]['name'], 'json_data':json_data, 'topictree_json_string':topictree_json_string, 'topictree_json_string_video':topictree_json_string_video}, context_instance=RequestContext(request))
 	except Exception as e:
 		print e
 		return HttpResponseRedirect("/inicio")
