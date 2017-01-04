0f8cdab56c0dc3616c779ee6fffe060800792e21
iuliusfdp <iuliusfdp@hotmail.com>
2017-01-04 18:06:12 -0300

    fix planificacion

--- planificacion/views.py
+++ planificacion/views.py
@@ -45,8 +45,7 @@ def getCurriculumProposed(request):
 	else:
 		isTeacher = False
 
-	if (request.user.has_perm('bakhanapp.isAdmin')):
-		classes = Class.objects.filter(id_institution_id=Teacher.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_institution_id')).values('level').distinct().order_by('level')
+	classes = Class.objects.filter(id_institution_id=Teacher.objects.filter(kaid_teacher=request.user.user_profile.kaid).values('id_institution_id')).values('level').distinct().order_by('level')
 	N = ['kinder','1ro basico','2do basico','3ro basico','4to basico','5to basico','6to basico','7mo basico','8vo basico','1ro medio','2do medio','3ro medio','4to medio']
 	for i in range(len(classes)):
 		#print classes[i]['level']
@@ -77,93 +76,8 @@ def getCurriculumProposed(request):
 			queryvideo = Video.objects.filter(id_video_name=miplan.videokhan[j]['id_video_id']).values('name_spanish', 'url_video')
 			miplan.videokhan[j]['namespanish']=queryvideo[0]['name_spanish']
 			miplan.videokhan[j]['urlvideo']=queryvideo[0]['url_video']
-
-	chapter = Chapter_Mineduc.objects.all()
-	mision=[]
-	for chap in chapter:
-		capitulo={}
-		capitulo["id"]=chap.id_chapter_mineduc
-		capitulo["nombre"]=chap.name
-		unidad=[]
-		topic = Topic_Mineduc.objects.filter(id_chapter_id=chap.id_chapter_mineduc)
-		for top in topic:
-			topico={}
-			topico["id"]=top.id_topic_mineduc
-			topico["nombre"]=top.name
-			aprendizaje=[]
-			subtopic = Subtopic_Mineduc.objects.filter(id_topic_id=top.id_topic_mineduc).order_by('name')
-			for sub in subtopic:
-				subtopico={}
-				subtopico["id"]=sub.id_subtopic_mineduc
-				subtopico["nombre"]=sub.name
-				subtopico["aeoa"]=sub.AE_OE
-				skills=[]
-				videos=[]
-				subtopicskill = Subtopic_Skill_Mineduc.objects.filter(id_subtopic_mineduc_id=subtopico["id"])
-				for subskill in subtopicskill:
-					nameskill = Skill.objects.filter(id_skill_name=subskill.id_skill_name_id).values('name_spanish', 'url_skill')
-					subtskillmin={}
-					subtskillmin["skill"]=subskill.id_skill_name_id
-					subtskillmin["nombre"]=nameskill[0]['name_spanish']
-					subtskillmin["url"]=nameskill[0]['url_skill']
-					subtskillmin["idtree"]=subskill.id_tree
-					skills.append(subtskillmin)
-				subtopico["skills"]=skills
-				subtopicvideo = Subtopic_Video_Mineduc.objects.filter(id_subtopic_name_mineduc_id=subtopico["id"])
-				for subvideo in subtopicvideo:
-					namevideo=Video.objects.filter(id_video_name=subvideo.id_video_name_id).values('name_spanish', 'url_video')
-					subtvideomin={}
-					subtvideomin["video"]=subvideo.id_video_name_id
-					subtvideomin["nombre"]=namevideo[0]['name_spanish']
-					subtvideomin["url"]=namevideo[0]['url_video']
-					subtvideomin["idtree"]=subvideo.id_tree
-					videos.append(subtvideomin)
-				subtopico["videos"]=videos
-				aprendizaje.append(subtopico)
-			topico["subtopico"]=aprendizaje
-			unidad.append(topico)
-		capitulo["topico"]=unidad
-		mision.append(capitulo)
-	json_dict={"capitulos":mision}
-	json_data = json.dumps(json_dict)
-
-	topictree_json={}
-	topictree_json['checkbox']={'keep_selected_style':False}
-	topictree_json['plugins']=['checkbox','search']
-	topictree=[]
-	subjects=Subject.objects.all()
-	for subject in subjects:
-	    subject_obj={"id": subject.id_subject_name, "parent":"#", "text": subject.name_spanish, "state": {"opened":"true"}, "icon":"false"}
-	    topictree.append(subject_obj)
-	subject_chapter=Chapter.objects.exclude(index=None).order_by('index')
-	for chapter in subject_chapter:
-	    chapter_obj={"id":chapter.id_chapter_name, "parent": chapter.id_subject_name_id, "text":chapter.name_spanish, "icon":"false"}
-	    topictree.append(chapter_obj)
-	chapter_topic=Topic.objects.exclude(index=None).order_by('index')
-	for topic in chapter_topic:
-	    topic_obj={"id":topic.id_topic_name, "parent": topic.id_chapter_name_id, "text":topic.name_spanish, "icon":"false"}
-	    topictree.append(topic_obj)
-	topic_subtopic=Subtopic.objects.exclude(index=None).order_by('index')
-	for subtopic in topic_subtopic:
-	    subtopic_obj={"id":subtopic.id_subtopic_name, "parent": subtopic.id_topic_name_id, "text":subtopic.name_spanish, "icon":"false"}
-	    topictree.append(subtopic_obj)
-	subtopic_skill=Subtopic_Skill.objects.filter(id_subtopic_name_id__in=topic_subtopic).select_related('id_skill_name')
-	id=0
-	for skill in subtopic_skill:
-	    skill_id=skill.id_subtopic_skill
-	    skill_obj={"id":skill_id, "parent":skill.id_subtopic_name_id, "text": skill.id_skill_name.name_spanish, "data":{"skill_id":skill.id_skill_name.id_skill_name}, "icon":"false", "index":skill.id_skill_name.index}
-	    sorted(skill_obj, key=skill_obj.get)
-	    topictree.append(skill_obj)
-	    id=id+1
-	subtopic_video=Subtopic_Video.objects.filter(id_subtopic_name_id__in=topic_subtopic).select_related('id_video_name')
-	for video in subtopic_video:
-		video_id=video.id_subtopic_video
-		video_obj={"id":video_id, "parent":video.id_subtopic_name_id, "text": video.id_video_name.name_spanish, "data":{"video_id":video.id_video_name.id_video_name}, "index":video.id_video_name.index}
-		sorted(video_obj, key=video_obj.get)
-		topictree.append(video_obj)
-	topictree_json['core']={'data':topictree}
-	topictree_json_string=json.dumps(topictree_json)
-	'''
+		'''
+	
 	return render_to_response('planificacion.html', { 'isTeacher':isTeacher, 'classes':classes} ,context_instance=RequestContext(request))
 
 
@@ -171,10 +85,31 @@ def getCurriculumProposed(request):
 def getCurriculumPropuesto(request, level):
 	request.session.set_expiry(timeSleep)
 	try:
+		request.session.set_expiry(timeSleep)
+		if (Class_Subject.objects.filter(kaid_teacher=request.user.user_profile.kaid)):
+			isTeacher = True
+		else:
+			isTeacher = False
 		N = ['kinder','1ro basico','2do basico','3ro basico','4to basico','5to basico','6to basico','7mo basico','8vo basico','1ro medio','2do medio','3ro medio','4to medio']
 		nivel = N[int(level)]
 
 		tpmin = Topic_Mineduc.objects.filter(id_chapter_id=Chapter_Mineduc.objects.filter(name__iexact=nivel))
+		print request.user.user_profile.kaid
+		print nivel
+		miplanificacion = Planning.objects.filter(teacher=request.user.user_profile.kaid, curso__iexact=nivel)
+		#print miplanificacion
+		for miplan in miplanificacion:
+			miplan.ejerciciokhan = Skill_Planning.objects.filter(id_planning_id=miplan.id_planning).values('id_skill_id')
+			for i in range(len(miplan.ejerciciokhan)):
+				queryskill = Skill.objects.filter(id_skill_name=miplan.ejerciciokhan[i]['id_skill_id']).values('name_spanish', 'url_skill')
+				miplan.ejerciciokhan[i]['namespanish']=queryskill[0]['name_spanish']
+				miplan.ejerciciokhan[i]['urlskill']=queryskill[0]['url_skill']
+			miplan.videokhan = Video_Planning.objects.filter(id_planning_id=miplan.id_planning).values('id_video_id')
+			for j in range(len(miplan.videokhan)):
+				queryvideo = Video.objects.filter(id_video_name=miplan.videokhan[j]['id_video_id']).values('name_spanish', 'url_video')
+				miplan.videokhan[j]['namespanish']=queryvideo[0]['name_spanish']
+				miplan.videokhan[j]['urlvideo']=queryvideo[0]['url_video']
+
 
 		chapter = Chapter_Mineduc.objects.all()
 		mision=[]
@@ -293,7 +228,7 @@ def getCurriculumPropuesto(request, level):
 		topictree_json_vid['core']={'data':topictree_vid}
 		topictree_json_string_video=json.dumps(topictree_json_vid)
 
-		return render_to_response('planificacionnivel.html',{'nivel':nivel, 'topic_mineduc':tpmin, 'json_data':json_data, 'topictree_exercise':topictree_json_string_exercise, 'topictree_video':topictree_json_string_video},  context_instance=RequestContext(request))
+		return render_to_response('planificacionnivel.html',{'nivel':nivel, 'topic_mineduc':tpmin, 'json_data':json_data, 'topictree_exercise':topictree_json_string_exercise, 'topictree_video':topictree_json_string_video, 'isTeacher':isTeacher, 'miplanificacion':miplanificacion},  context_instance=RequestContext(request))
 	except Exception as e:
 		print e
 		return HttpResponseRedirect("/inicio")
